b9dc700949a5ae3d6a4b462902157c448a36fbcd
Souhaila_Ben_Allal <Souhaila.ben.allal@student.ehb.be>
2021-01-04 15:24:09 +0100

    CRUD data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
+import express from 'express'
+import bodyParser from 'body-parser'
 
-const port = 3020;
+import usersRoutes from './routes/users.js'
+ 
+const app = express()
+const port = 3030;
+
+app.use(bodyParser.json())
+app.use('/users', usersRoutes) 
 
 app.listen(port,() =>{
     console.log(`Example app listenting at http://localhost:${port}`)
 })
+
+app.get('/', (req,res) =>{
+    console.log('[TEST]!')
+    res.send('Hompage')
+})
+
 app.get('/user', function (req, res) { 
     res.send('Hello World!')
 })
