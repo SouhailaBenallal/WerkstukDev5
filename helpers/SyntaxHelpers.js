a3f7b1117d117e5018865644e5eebfea1bef8854
Souhaila_Ben_Allal <Souhaila.ben.allal@student.ehb.be>
2021-01-17 13:10:15 +0100

    Comment test

--- helpers/SyntaxHelpers.js
+++ helpers/SyntaxHelpers.js
@@ -1,26 +1,26 @@
-const syntaxHelpers = {
+// const syntaxHelpers = {
 
-    capitalFirstCharacter: (userInput) =>{
-        if(userInput != "" && userInput != null){
-            if(typeof userInput !== 'number'){
-                if(userInput.length > 1){
-                    return string.charAt(0).toUpperCase() + string.slice(1)
-                }
-            }
-        }
-        return null
-    },
-    replacePunctuation: () =>
-    {
-        if(userInput != "" && userInput != null){
-            if(typeof userInput !== 'number'){
-                if(userInput.length > 1){
-                    return string.charAt(0).replace(/,/g,'.') + string.slice(1)
-                }
-            }
-        }
-        return null
-    },
+//     capitalFirstCharacter: (userInput) =>{
+//         if(userInput != "" && userInput != null){
+//             if(typeof userInput !== 'number'){
+//                 if(userInput.length > 1){
+//                     return string.charAt(0).toUpperCase() + string.slice(1)
+//                 }
+//             }
+//         }
+//         return null
+//     },
+//     replacePunctuation: () =>
+//     {
+//         if(userInput != "" && userInput != null){
+//             if(typeof userInput !== 'number'){
+//                 if(userInput.length > 1){
+//                     return string.charAt(0).replace(/,/g,'.') + string.slice(1)
+//                 }
+//             }
+//         }
+//         return null
+//     },
 
-}
-export default syntaxHelpers
+// }
+// export default syntaxHelpers
